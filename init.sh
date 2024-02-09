24c8f2a69235cdca281669901a333a770a175eac
Jordan Shatford <jordanshatford@live.com>
2024-02-09 12:04:27 +1100

    feat: init script now ensures env file is present and used
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# This script creates the folder structure required by the docker-compose file.
+
+# Ensure that an env file is present, otherwise copy the sample env file.
+env_file=".env"
+if ! [ -f $env_file ]; then
+  cp example.env $env_file
+fi
+
+# Source the env file so we know where to create the directories required.
+source $env_file
+root_dir="${ROOT_DIRECTORY:-.}"
 
 dirs=(
   "data/dockerdata"
@@ -9,7 +18,8 @@ dirs=(
   "data/torrents/tv"
 )
 
+# Create each of the required directories.
 for dir in "${dirs[@]}"; do
-  mkdir -p "$dir"
+  mkdir -p "$root_dir/$dir"
   touch "$dir/.gitkeep"
 done
